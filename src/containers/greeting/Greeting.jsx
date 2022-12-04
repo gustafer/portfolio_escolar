7e9ecd8085e97ea019536042e2519f5bc4392535
gustafer <gustavobreasy@gmail.com>
2022-12-04 19:52:09 -0300

    changed to paylist

--- src/containers/greeting/Greeting.jsx
+++ src/containers/greeting/Greeting.jsx
@@ -75,7 +75,7 @@ export default function Greeting() {
             </div>
             <div className="card__face--back">
               <iframe style={{borderRadius:"12px"}}
-                src="https://open.spotify.com/embed/track/5w4KDy9LBWjK8nm7JwKMLh?utm_source=generator"
+                src="https://open.spotify.com/embed/playlist/6Xf2m3HRNDWz6HOWwuU1dt?utm_source=generator"
                 width="50%" height="352" frameBorder="0" allowFullScreen=""
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
               </iframe>
